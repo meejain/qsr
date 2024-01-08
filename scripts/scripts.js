60af1193682f26d2e8303ad186205e12ec63921a
meejain <37831850+meejain@users.noreply.github.com>
2024-01-08 18:50:22 +0530

    Preparing sample1 for the qsr (#3)
    * Preparing for the sample
    
    * Fixing the hero & quote
    
    * Fixing the hero & quote
    
    * Updating the Crosswalk section
    
    * Updating the Crosswalk section
    
    * Updating the Crosswalk section
    
    * Updating the narrative section
    
    * Updating the narrative section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,6 +11,7 @@ import {
   waitForLCP,
   loadBlocks,
   loadCSS,
+  createOptimizedPicture,
 } from './aem.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
@@ -28,6 +29,42 @@ function buildHeroBlock(main) {
     section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
   }
+}
+
+function decorateSectionsWithBackgrounds(element) {
+  const sections = element.querySelectorAll(`.section[data-bg-image],
+  .section[data-bg-image-desktop],
+  .section[data-bg-image-mobile],
+  .section[data-bg-image-tablet]`);
+  sections.forEach((section) => {
+    const bgImage = section.getAttribute('data-bg-image');
+    const bgImageDesktop = section.getAttribute('data-bg-image-desktop');
+    const bgImageMobile = section.getAttribute('data-bg-image-mobile');
+    const bgImageTablet = section.getAttribute('data-bg-image-tablet');
+    const viewPort = window.deviceType;
+    let background;
+    switch (viewPort) {
+      case 'Mobile':
+        background = bgImageMobile || bgImageTablet || bgImageDesktop || bgImage;
+        break;
+      case 'Tablet':
+        background = bgImageTablet || bgImageDesktop || bgImage || bgImageMobile;
+        break;
+      default:
+        background = bgImageDesktop || bgImage || bgImageTablet || bgImageMobile;
+        break;
+    }
+    if (background) {
+      if (section.classList.contains('with-static-background-image')) {
+        section.classList.add('with-static-background-image');
+      } else {
+        section.classList.add('with-background-image');
+      }
+      const backgroundPic = createOptimizedPicture(background);
+      backgroundPic.classList.add('background-image');
+      section.append(backgroundPic);
+    }
+  });
 }
 
 /**
@@ -67,6 +104,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  decorateSectionsWithBackgrounds(main);
 }
 
 /**
